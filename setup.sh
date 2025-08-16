defdc1df3313e067ba75deb12a34b86578f075cc
Yeonwoo Jang <yjang385@gmail.com>
2025-08-16 06:11:19 +0000

    add support for gpt-oss

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ apt-get update && apt-get install -y vim
 curl -LsSf https://astral.sh/uv/install.sh | sh
 . $HOME/.local/bin/env
 
-uv venv --python=3.10
+uv venv --python=3.12
 . .venv/bin/activate
 
 uv pip install torch transformers accelerate datasets
@@ -16,7 +16,15 @@ uv pip install python-dotenv pydra-config
 uv pip install together openai anthropic google-generativeai
 uv pip install ninja
 uv pip install hydra-core omegaconf
-uv pip install -e .
+uv pip install unsloth
+uv pip install wandb
+# uv pip install -e .
+
+# if gpt-oss
+# uv pip install --pre vllm==0.10.1+gptoss \
+#     --extra-index-url https://wheels.vllm.ai/gpt-oss/ \
+#     --extra-index-url https://download.pytorch.org/whl/nightly/cu128 \
+#     --index-strategy unsafe-best-match
 
 apt-get update && apt-get install -y python3.10-dev build-essential
 
