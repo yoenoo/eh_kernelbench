e1be4ed66beffffb5b9997c0bcc17e4545de1e9d
Yeonwoo Jang <yjang385@gmail.com>
2025-08-17 10:52:49 +0000

    trl grpo multi-gpu scripts

---
+++ grpo_bare_bone/start_grpo_train.sh
@@ -0,0 +1,8 @@
+set -xe
+
+CUDA_DEVICE_ORDER=PCI_BUS_ID 
+NCCL_ASYNC_ERROR_HANDLING=1 
+CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch \
+  --num_processes=4 \
+  --mixed_precision=bf16 \
+  grpo_bare_bone/grpo_train.py
