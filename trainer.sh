55250bf74df496e58dc3f51cda4e4ac3b61c97d5
Yeonwoo Jang <yjang385@gmail.com>
2025-08-20 16:45:12 +0000

    add launch scripts

--- trainer.sh
+++ trainer.sh
@@ -3,6 +3,7 @@ set -xe
 CUDA_DEVICE_ORDER=PCI_BUS_ID 
 NCCL_ASYNC_ERROR_HANDLING=1 
 
-VLLM_LOGGING_LEVEL=WARNING CUDA_VISIBLE_DEVICES=0,1 accelerate launch \
-  --num_processes=2 \
-  --module src.train.trainer
+VLLM_LOGGING_LEVEL=WARNING CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch \
+  --num_processes=4 \
+  --module src.train.trainer \
+  --config-name rl_locking
